41459d4fbeac7a14c917ac81918f9d4a3264bd1d
Alice <alicenjoroge707@gmail.com>
2020-02-13 17:11:14 +0300

    arrow functions

--- scripts/first.js
+++ scripts/first.js
@@ -1,7 +1,18 @@
 document.getElementById("id1").addEventListener("click", myFunction);
-        
+
 function myFunction() {
     document.getElementById('heading1').remove();
     document.getElementsByTagName('body')[0].classList.add("bodyStyle");
     document.getElementById('demo').innerHTML = 'I am learning Javascript';
 }
+/*
+function hello() {
+    return "hello world";
+} */
+
+hello = () => {
+    return "hello short world!"
+};
+
+
+document.getElementById('hello').innerText = hello();
